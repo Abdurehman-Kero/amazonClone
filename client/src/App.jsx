aa905b1ddc51a6fda50b86c70024c788ff85b6ac
Abdurehman Kero <keroabdurehman@gmail.com>
2025-12-08 21:49:56 +0300

    caruosel added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Components/Header/Header'
+import Carouse from './Components/Carousel/Carousel'
+import CarouselEffect from './Components/Carousel/Carousel'
 
 function App() { 
   return (
     <>
        <Header/>
+       <CarouselEffect />
     </>
   )
 }
---
+++ client/src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import classes from "./carousel.module.css"
+
+import { img } from "./img/data"; 
+const CarouselEffect = () => {
+  return (
+    <div>
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showIndicators={true}
+        showThumbs={false}
+      >
+
+      {img.map((imageItemLink, key) => {
+          return <img key={key} src={imageItemLink} />;
+        })}
+        </Carousel>
+        <div className={classes.hero_img}></div>
+    </div>
+  );
+};
+
+export default CarouselEffect;
