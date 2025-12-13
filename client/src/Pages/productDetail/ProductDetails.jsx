27acbd22dd9dad9563abff31d0f0c0da339a6d41
Abdurehman Kero <keroabdurehman@gmail.com>
2025-12-13 09:47:26 +0300

    product detail page routing added

--- client/src/Pages/productDetail/ProductDetails.jsx
+++ client/src/Pages/productDetail/ProductDetails.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useParams } from "react-router-dom"; 
+import { useEffect, useState } from "react";
+import axios from "axios"; 
+// import Spinner from "../../components/Loading/Spinner";
+import Layout from "../../Layout/Layout";
+import ProductCard from "../../Components/Product/ProductCard";
 
-const ProductDetails = () => {
+function ProductDetail() {
+  const { productId } = useParams();
+  const [product, setProduct] = useState({});
+  // const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    // setIsLoading(true);
+    axios
+      .get(`https://fakestoreapi.com/products/${productId}`)
+      .then((res) => {
+        setProduct(res.data);
+        // setIsLoading(false);
+      })
+      .catch((err) => {
+        // setIsLoading(false);
+        console.log(err);
+      });
+  }, []);
+  console.log(product);
   return (
-    <div>ProductDetails</div>
-  )
+    <Layout>
+      { <ProductCard product={product}   />}
+    </Layout>
+  );
 }
 
-export default ProductDetails
+export default ProductDetail;
