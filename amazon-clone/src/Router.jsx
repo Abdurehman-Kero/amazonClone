af3e7f44c49ebe39ad9e5289a306a562fd58bd48
Abdurehman Kero <keroabdurehman@gmail.com>
2025-12-23 11:07:34 +0300

    rename pages

--- amazon-clone/src/Router.jsx
+++ amazon-clone/src/Router.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing/Landing.jsx";
+import Landing from "./Pages/Landing/Landing.jsx";
 
 import Cart from "./pages/Cart/Cart.jsx";
 import Result from "./Pages/Results/Resulst";
