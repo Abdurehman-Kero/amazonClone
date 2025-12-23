5b28ef8f148f618c1109557dc927d64946794af1
Abdurehman Kero <keroabdurehman@gmail.com>
2025-12-23 10:51:21 +0300

    Frontend deployment using Netlify

--- amazon-clone/src/api/axios.js
+++ amazon-clone/src/api/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-project-82590/us-central1/api",
+  //local instance of firebase function
+  // baseURL: "http://127.0.0.1:5001/clone-project-82590/us-central1/api",
+  //deployed version of amazon server on render.com
+  baseURL: "https://amazon-clone-2025-g242.onrender.com",
 });
 export {axiosInstance};
--- amazon-clone/vite.config.js
+++ amazon-clone/vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/amazon-Clone-2025",
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
+  plugins: [react()],
+  base: "/",
 });
