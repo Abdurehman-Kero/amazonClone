a5f6aa882f8b00d8560b5af685efe0d169a40435
Abdurehman Kero <keroabdurehman@gmail.com>
2025-12-23 11:39:50 +0300

    main.jsx is modified

--- amazon-clone/src/main.jsx
+++ amazon-clone/src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import DataProvider from "./Components/Context/DataProvider.jsx";
- 
- 
+
 import { initialState, reducer } from "./utils/reducer.js";
 
+// Use Vite's BASE_URL environment variable to set basename dynamically
+const basename = import.meta.env.BASE_URL || "/";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/amazon-Clone-2025">
+  <BrowserRouter basename={basename}>
     <DataProvider reducer={reducer} initialState={initialState}>
       <App />
     </DataProvider>
